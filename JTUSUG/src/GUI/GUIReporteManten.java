cad2c5796ae768d69b6f33e011bb784f88bc5752
Guillermo Garcia Pascual <memopein97@gmail.com>
2018-06-03 21:46:04 -0500

    galeria de rutas y reportes de mantenimientos funcionando

--- JTUSUG/src/GUI/GUIReporteManten.java
+++ JTUSUG/src/GUI/GUIReporteManten.java
@@ -1,7 +1,6 @@
 package GUI;
 
 import CONTROLLERS.ControladorMantenimiento;
-import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.*;
 import java.awt.*;
