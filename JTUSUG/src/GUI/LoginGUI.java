cf329f1f4212952da61867ee247d8a9119688060
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-06-04 02:59:46 -0500

    Newer aLV

--- JTUSUG/src/GUI/LoginGUI.java
+++ JTUSUG/src/GUI/LoginGUI.java
@@ -96,6 +96,7 @@ public class LoginGUI {
 
     public void iniciarSesion() throws Exception {
         cargarSesion();
+        //System.err.println(contrasenia + " - " + Encriptar.md5(txt_password.getText()));
         if (contrasenia.equals(
                 Encriptar.md5(txt_password.getText()))) {
             RootGUI main = new RootGUI(rol, nombre_usuario);
