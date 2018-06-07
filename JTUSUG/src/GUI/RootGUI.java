fd7e4d7fbb571efdab4591ec825b5b003e3f1206
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-06-06 23:55:58 -0500

    Run alv

--- JTUSUG/src/GUI/RootGUI.java
+++ JTUSUG/src/GUI/RootGUI.java
@@ -2,15 +2,21 @@ package GUI;
 
 import CONTROLLERS.Conexion;
 import GaleriaRutas.interfaz;
+import Servicios.FabricaComponentes;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 /**
@@ -21,17 +27,22 @@ public class RootGUI {
     //Datos de la sesion
     String rol_activo;
     String nombre_usuario;
+    String urlimage;
+    private JMenuBar bar_menu;
+    private JMenu ar;
+    private JMenuItem   menu_cerrarsesion;
+    private JMenuItem   menu_cambiarpass;
     
     CustomActionListener listen;
     String carpeta_img = "src/imagenes/";
     Font font;
     public static JFrame root;
     public static JPanel panel;
-    public JButton btn_close, btn_secre, btn_almacen, btn_manten, btn_rrhh, btn_rutas;
+    public JButton btn_close, btn_secre, btn_manten, btn_rrhh, btn_rutas;
     
     //Para reciclar
     JLabel lb_text;
-    private JLabel lb_title, lb_descripcion;
+    private JLabel lb_title, lb_descripcion, lb_img_user;
     // Botones para la secretaria    
     private JButton btn_regresar, btn_listaBus, btn_facturas, btn_reportes;
     // Botones para almacenista
@@ -41,15 +52,16 @@ public class RootGUI {
     // Botones de Mantenimiento
     private JButton btn_nuevoreporte, btn_reUnidades, btn_historial;
 
-    public RootGUI(String rol, String nombreUser){
+    public RootGUI(String rol, String nombreUser, String urlimg){
         font = new Font("Segoe UI", Font.PLAIN, 14);
         listen = new CustomActionListener();
-        this.rol_activo = rol;
+        this.rol_activo     = rol;
         this.nombre_usuario = nombreUser;
+        this.urlimage       = urlimg;
         initComponents();
         switch(rol){            
             case "root":
-                habilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                habilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                 break;
             case "secretaria":
                 habilitarComponentes(btn_regresar, btn_listaBus, btn_facturas, btn_reportes);
@@ -70,7 +82,7 @@ public class RootGUI {
         if (!rol_activo.equals("root"))btn_regresar.setVisible(false);
     }
     public RootGUI() {
-        this("", "");
+        this("root", "Alejo", "src/imagenes/icono usuario.jpeg");
     }
 
     public void initComponents() {
@@ -79,16 +91,37 @@ public class RootGUI {
         panel =                 Builder.crearPanel(     root,       new Rectangle(0, 0, 700, 600),      "src/imagenes/pagina_de_fondo.png", true);
         lb_title =              Builder.crearLabel(     panel, "",               new Rectangle(317, 202, 200, 40), Color.BLUE,   null, font);        
         //lb_descripcion =        Builder.crearLabel(     panel, "descripcion",    new Rectangle(114, 445, 100, 40), null,         null, font);
-        lb_text =               Builder.crearLabel(     panel, "Seleccione el icono de la seccion que desea visitar",   new Rectangle(193, 172, 300, 60), null, null, font);        
+        lb_img_user       =     FabricaComponentes.crearLabelImg(panel, "biv", new Rectangle(547,109, 32, 32), urlimage, true);
+        lb_text =               Builder.crearLabel(     panel, "Seleccione el icono de la seccion que desea visitar",   new Rectangle(193, 172, 300, 60), null, null, font);
+        
+        bar_menu        =       new JMenuBar();//FabricaComponentes.crearMenuBar(panel, "b", new Rectangle(589,116, 67,18));
+        panel.add(bar_menu);        
+        bar_menu.       setBounds(new Rectangle(579,116, 77,18));
+        bar_menu.       setBackground(Color.gray);
+        ar = new JMenu(nombre_usuario);
+        
+        menu_cerrarsesion   =   new JMenuItem("Cerrar Sesion");        
+        menu_cambiarpass    =   new JMenuItem("Cambiar Password");
+        menu_cambiarpass.addActionListener(listen);
+        menu_cerrarsesion.addActionListener(listen);
+        
+        //bar_menu.addMouseListener(l);
+        //menu_cerrarsesion.addItemListener(listenItems);
+        ar.add(menu_cambiarpass);
+        ar.add(menu_cerrarsesion);
+        
+        bar_menu.add(ar);
+        
+        
         // Botones generales
         btn_regresar =          Builder.crearButtonIcon(panel, "Uncknown",          carpeta_img + "regresar.png",               new Rectangle(335, 523, 32,  32), listen, true, false,false);
         btn_close =             Builder.crearButtonIcon(panel, "CerrarSesion",      carpeta_img + "cerrar_sesion.png",          new Rectangle(460, 506, 201, 63), listen, true, true, true);
         // Botones de root
         btn_secre =             Builder.crearButtonIcon(panel, "btnSecretaria",     carpeta_img + "secretaria.png",             new Rectangle(85,  256, 256, 63), listen, true, true, false);
-        btn_almacen =           Builder.crearButtonIcon(panel, "btnAlmacen",        carpeta_img + "almacen.png",                new Rectangle(85,  352, 256, 63), listen, true, true, false);
+        //           Builder.crearButtonIcon(panel, "btnAlmacen",        carpeta_img + "almacen.png",                new Rectangle(85,  352, 256, 63), listen, true, true, false);
         btn_manten =            Builder.crearButtonIcon(panel, "btnManten",         carpeta_img + "mantenimiento.png",          new Rectangle(379, 256, 256, 63), listen, true, true, false);        
         btn_rrhh =              Builder.crearButtonIcon(panel, "btnRRHH",           carpeta_img + "recursos_humanos.png",       new Rectangle(379, 352, 256, 63), listen, true, true, false);
-        btn_rutas =             Builder.crearButtonIcon(panel, "btnrutas",           carpeta_img + "rutas.png",                  new Rectangle(85 , 446, 256, 63), listen, true, true, true);
+        btn_rutas =             Builder.crearButtonIcon(panel, "btnrutas",           carpeta_img + "rutas.png",                 new Rectangle(85, 352, 256, 63), listen, true, true, true);
         
         // Botones de Secretaria
         btn_listaBus =          Builder.crearButtonIcon(panel, "modulo_autobus",    carpeta_img + "lista_de_autobuses.png",     new Rectangle(85,  256, 256, 63), listen, false,true, false);
@@ -133,13 +166,13 @@ public class RootGUI {
                     root.dispose();
                     break;
                 case "btnSecretaria":
-                    deshabilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    deshabilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     habilitarComponentes(btn_regresar, btn_listaBus, btn_facturas, btn_reportes);
                     btn_regresar.setActionCommand("btn_regresar");
                     lb_title.setText("Secretaria");
                     break;
                 case "btnAlmacen":
-                    deshabilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    deshabilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     habilitarComponentes(btn_regresar, btn_insumos, btn_lista_invent, btn_gener_inventario);
                     btn_regresar.setActionCommand("btn_back_almacen");
                     lb_title.setText("Almacen");
@@ -154,13 +187,13 @@ public class RootGUI {
                     break;
                     
                 case "btnManten":
-                    deshabilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    deshabilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     habilitarComponentes(btn_regresar, btn_nuevoreporte, btn_reUnidades, btn_historial);
                     btn_regresar.setActionCommand("btn_back_Mant");
                     lb_title.setText("Mantenimiento");
                     break;
                 case "btnRRHH":
-                    deshabilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    deshabilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     habilitarComponentes(btn_regresar, btn_trabajadores, btn_expedientes, btn_nuevosempleado, btn_iactivos);
                     btn_regresar.setActionCommand("btn_back_RH");
                     lb_title.setText("Recursos Humanos");
@@ -168,20 +201,20 @@ public class RootGUI {
                 // Btns de Regresar segun el caso
                 case "btn_regresar": //Secretaria
                     deshabilitarComponentes(btn_regresar, btn_listaBus, btn_facturas, btn_reportes);
-                    habilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    habilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     
                     break;
                 case "btn_back_almacen":
                     deshabilitarComponentes(btn_regresar, btn_insumos, btn_lista_invent, btn_gener_inventario);
-                    habilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    habilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     break;
                 case "btn_back_RH":
                     deshabilitarComponentes(btn_regresar, btn_trabajadores, btn_expedientes, btn_nuevosempleado, btn_iactivos);
-                    habilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    habilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     break;
                 case "btn_back_Mant":
                     deshabilitarComponentes(btn_regresar, btn_nuevoreporte, btn_reUnidades, btn_historial);
-                    habilitarComponentes(btn_secre, btn_almacen, btn_manten, btn_rrhh);
+                    habilitarComponentes(btn_secre, btn_manten, btn_rrhh);
                     break;
                 //  Botones de la Secretaria
                 case "modulo_autobus":
@@ -226,9 +259,32 @@ public class RootGUI {
                 case "Mhistorial":
                     HistorialMantenGUI h = new HistorialMantenGUI();
                     break;
+                    
+                 /** -------------------|Eventos para el JMenuBar|--------------------*/
+                case "Cerrar Sesion":
+                   new LoginGUI();
+                   root.dispose();
+                   break;
+                case "Cambiar Password":
+                    try{
+                        String newPass = javax.swing.JOptionPane.showInputDialog("Ingrese su nueva contraseña");
+                        String newPassConfirm  = javax.swing.JOptionPane.showInputDialog("Ingrese su nueva contraseña");
+                        if (newPass.equals(newPassConfirm)){
+                            
+                            javax.swing.JOptionPane.showMessageDialog(null, "Contraseña cambiada");
+                        }
+                    }catch(NullPointerException ex){
+                        
+                    }catch (Exception ex){
+                        
+                    }
+                   
+                   break;
             }
         }
     }
 
-
+    public void cambiarPasswordEvt(){
+        
+    }
 }
--- JTUSUG/src/Servicios/FabricaComponentes.java
+++ JTUSUG/src/Servicios/FabricaComponentes.java
@@ -4,8 +4,14 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.KeyListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.text.JTextComponent;
@@ -15,41 +21,122 @@ import javax.swing.text.JTextComponent;
  * @author Alekhius
  */
 public class FabricaComponentes {
+
     /**
-     * 
+     *
      * @param ui
      * @param params
      * @param args
-     * @return 
+     * @return
      */
-    public static JTextField crearTextField(Container ui, String params, Object... args){
+    public static JTextField crearTextField(Container ui, String params, Object... args) {
         JTextField producto = new JTextField();
         ui.add(producto);
-        
+
         int index = 0;
         addAtribs(producto, params, args);
-        for (Character op: params.toCharArray()){
-            switch (op){
-                case 'T': producto.setText	( (String)args[index++]); break;
-                case 'K': producto.addKeyListener((KeyListener)args[index++]); break;
-                default: index++;
+        for (Character op : params.toCharArray()) {
+            switch (op) {
+                case 'T':
+                    producto.setText((String) args[index++]);
+                    break;
+                case 'K':
+                    producto.addKeyListener((KeyListener) args[index++]);
+                    break;
+                default:
+                    index++;
             }
         }
         return producto;
     }
-    
-    private static void addAtribs(Component producto, String params, Object... args){
+
+    private static void addAtribs(Component producto, String params, Object... args) {
         int index = 0;
-        for (Character op: params.toCharArray()){
-            switch (op){
-                case 'b': producto.setBounds    ( (Rectangle)args[index++]); break;
-                case 'v': producto.setVisible   ( (Boolean)args[index++]); break;
-                case 'e': producto.setEnabled   ( (Boolean)args[index++]); break;
-                case 'f': producto.setFont      ( (Font)args[index++]); break;
-                case 'c': producto.setForeground( (Color)args[index++]); break;
-                case 'g': producto.setBackground( (Color)args[index++]); break;
-                default: index++;
+        for (Character op : params.toCharArray()) {
+            switch (op) {
+                case 'b':
+                    producto.setBounds((Rectangle) args[index++]);
+                    break;
+                case 'v':
+                    producto.setVisible((Boolean) args[index++]);
+                    break;
+                case 'e':
+                    producto.setEnabled((Boolean) args[index++]);
+                    break;
+                case 'f':
+                    producto.setFont((Font) args[index++]);
+                    break;
+                case 'c':
+                    producto.setForeground((Color) args[index++]);
+                    break;
+                case 'g':
+                    producto.setBackground((Color) args[index++]);
+                    break;
+                default:
+                    index++;
             }
         }
+    }
+
+    /**
+     *
+     * @param ui
+     * @param params i = Image
+     * @param args
+     * @return
+     */
+    public static JLabel crearLabelImg(Container ui, String params, Object... args) {
+        JLabel producto = new JLabel();
+        ui.add(producto);
+
+        int index = 0;
+        addAtribs(producto, params, args);
+        for (Character op : params.toCharArray()) {
+            switch (op) {
+                case 'i':
+                    String Path = (String) args[index++];//.replace('\u005C\u005C', '\u002F'));
+                    System.err.println(Path);
+                    ImageIcon fot = new ImageIcon(Path.replace('\u005C\u005C', '\u002F'));
+                    Icon icon = new ImageIcon(fot.getImage().getScaledInstance(producto.getWidth(), producto.getHeight(), Image.SCALE_DEFAULT));
+                    //Icon icon = new ImageIcon(Path);
+                    producto.setIcon(icon);
+                    break;
+                default:
+                    index++;
+            }
+        }
+        return producto;
+    }
+
+    public static JMenuBar crearMenuBar(Container ui, String params, Object... args) {
+        JMenuBar producto = new JMenuBar();
+        ui.add(producto);
+
+        int index = 0;
+        addAtribs(producto, params, args);
+        for (Character op : params.toCharArray()) {
+            switch (op) {
+                case 'o':
+                    producto.setOpaque((Boolean)args[index++]);
+                    break;
+            }
+        }
+        return producto;
+    }
+
+    public static JMenu crearMenu(Container ui, String params, Object... args) {
+        JMenu producto = new JMenu();
+        ui.add(producto);
+
+        int index = 0;
+        addAtribs(producto, params, args);
+        for (Character op : params.toCharArray()) {
+            switch (op) {
+                case 'T':
+                    producto.setText((String)args[index++]);
+                    break;
+            }
+        }
+        return producto;
     }
 }
