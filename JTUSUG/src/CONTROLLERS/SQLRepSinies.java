97e0eb219501228965c2c5f8155175e063fe1dd8
Guillermo Garcia Pascual <memopein97@gmail.com>
2018-06-10 01:07:43 -0500

    listar siniestros andando

--- JTUSUG/src/CONTROLLERS/SQLRepSinies.java
+++ JTUSUG/src/CONTROLLERS/SQLRepSinies.java
@@ -30,146 +30,44 @@ public class SQLRepSinies {
             conn = Conexion.getConexion();
      }
      
-     public String[][] obtenerRegistro(){
-        String sql = "select count(codigo_accidente) as total from sistemaTusug.siniestro ";
-            PreparedStatement pst;
-            ResultSet res;
-            int n = 0;
-        try {
-            pst = conn.prepareStatement(sql);
-        
-            res = pst.executeQuery();
-            res.next();
-            n = res.getInt("total");
-            res.close();
-            } catch (SQLException ex) {
-            Logger.getLogger(SQLAutobus.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        String tabla[][] = new String[n][4];
-        
+     public void llenarTable(){
         try{
-            sql = "select codigo_accidente from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index = 0;
-            while(res.next()){
-                String dato=res.getString("codigo_accidente");
-                tabla[index][0] = dato;
-                index++;
-                System.out.println(dato);
+            
+            Object datos[] = new Object[3];
+            String sql ="select codigo_accidente, siniestro, tipo from sistemaTusug.siniestro";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+        
+            ResultSet rs = pstm.executeQuery();
+            while(rs.next()){
+                datos[0]=rs.getInt(1);
+                datos[1]=rs.getDate(2);
+                datos[2]=rs.getString(3);
+              
+                interfaz.model.addRow(datos);
             }
-            sql = "select siniestro from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index2 = 0;
-            while(res.next()){
-                String dato2=res.getString("siniestro");
-                tabla[index2][1] = dato2;
-                index2++;
-            }
-            sql = "select tipo from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index3 = 0;
-            while(res.next()){
-                String dato3=res.getString("tipo");
-                tabla[index3][2] = dato3;
-                index3++;
-            }
-            sql = "select estado from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index4 = 0;
-            while(res.next()){
-                String dato4=res.getString("estado");
-                tabla[index4][3] = dato4;
-                index4++;
-            }
-            res.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(SQLAutobus.class.getName()).log(Level.SEVERE, null, ex);
+        }catch (Exception e) {
+            //System.out.println(e.getMessage());
+            Logger.getLogger(SQLAutobus.class.getName()).log(Level.SEVERE, null, e);
         }
-        return tabla;
     }
-        public void creaRepor() throws JRException{
+    
+         public void creaRepor() throws JRException{
                try{
           
             Map parametros = new HashMap();
             parametros.clear();
-            parametros.put("holis", interfaz.valor );
+            parametros.put("holis", interfaz.valor);
             JasperReport reporte=JasperCompileManager.compileReport("ReporteSiniestro.jrxml");
             JasperPrint p= JasperFillManager.fillReport(reporte, parametros, conn);
             JasperViewer ventanavisor= new JasperViewer(p, false);
-            ventanavisor.setTitle("REPORTE DE Siniestro");
+            ventanavisor.setTitle("Expediente de Siniestros");
             ventanavisor.setVisible(true);
                }catch(HeadlessException | JRException e){
                      JOptionPane.showMessageDialog(null,"Error en el reporte"+ e);
-                 }
-               
+                 }              
                }
-     
-    public String[][] obtenerRegistroArgs(String args){
-        String sql = "select count(codigo_accidente) as total from sistemaTusug.siniestro ";
-            PreparedStatement pst;
-            ResultSet res;
-            int n = 0;
-        try {
-            pst = conn.prepareStatement(sql);
-        
-            res = pst.executeQuery();
-            res.next();
-            n = res.getInt("total");
-            res.close();
-            } catch (SQLException ex) {
-            Logger.getLogger(SQLAutobus.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        String tabla[][] = new String[n][3];
-        
-        try{
-            sql = "select codigo_accidente from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index = 0;
-            while(res.next()){
-                String dato=res.getString("codigo_accidente");
-                tabla[index][0] = dato;
-                index++;
-                System.out.print(dato);
-            }
-            sql = "select siniestro from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index2 = 0;
-            while(res.next()){
-                String dato2=res.getString("siniestro");
-                tabla[index2][1] = dato2;
-                index2++;
-            }
-            sql = "select tipo from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index3 = 0;
-            while(res.next()){
-                String dato3=res.getString("tipo");
-                tabla[index3][2] = dato3;
-                index3++;
-            }
-            sql = "select estado from sistemaTusug.siniestro ORDER BY codigo_accidente";
-            pst = conn.prepareStatement(sql);
-            res = pst.executeQuery();
-            int index4 = 0;
-            while(res.next()){
-                String dato4=res.getString("estado");
-                tabla[index4][3] = dato4;
-                index4++;
-            }
-            res.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(SQLAutobus.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return tabla;
-    } 
+
+ 
+    
 }
 
